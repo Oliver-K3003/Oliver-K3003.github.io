5a8f935fb5a7ca83621456140e3e83f4840066d3
Oliver-K3003 <Oliverkramer3003@gmail.com>
2022-12-08 16:26:47 -0500

    uploading finished portfolio site

---
+++ app.js
@@ -0,0 +1,10 @@
+const hamburger = document.querySelector(".hamburger");
+const burgerItems = document.querySelector(".hamburger-items");
+const card = document.querySelectorAll(".project-card");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+  hamburger.classList.toggle("active");
+  burgerItems.classList.toggle("show");
+}
