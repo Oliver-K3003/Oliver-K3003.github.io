c40be3e5faf94e47c679dc3f6398a11766ae43c9
Oliver-K3003 <Oliverkramer3003@gmail.com>
2022-12-08 19:45:32 -0500

    added final images

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const burgerItems = document.querySelector(".hamburger-items");
 const card = document.querySelectorAll(".project-card");
 
 hamburger.addEventListener("click", mobileMenu);
+burgerItems.addEventListener("click", mobileMenu);
 
 function mobileMenu() {
   hamburger.classList.toggle("active");
