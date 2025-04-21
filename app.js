008183ba0a4d39efba7280880b0357176bd0751e
Oliver-K3003 <Oliverkramer3003@gmail.com>
2025-04-21 19:33:36 -0400

    update with relevant information

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const hamburger = document.querySelector(".hamburger");
 const burgerItems = document.querySelector(".hamburger-items");
-const card = document.querySelectorAll(".project-card");
 
 hamburger.addEventListener("click", mobileMenu);
 burgerItems.addEventListener("click", mobileMenu);
 
 function mobileMenu() {
-  hamburger.classList.toggle("active");
-  burgerItems.classList.toggle("show");
+    hamburger.classList.toggle("active");
+    burgerItems.classList.toggle("show");
 }
